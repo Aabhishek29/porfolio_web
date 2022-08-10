adccb37c7572b5c9c64b6e85fc28776ce828d61c
Aabhishek29 <starkabhishek29@gmail.com>
2022-08-11 00:54:38 +0530

    procfile added 2

--- porfolio_web/settings.py
+++ porfolio_web/settings.py
@@ -23,14 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)_gz1iwy=xb54kj5!pxeo4cn*mv*hn15m!-%x9f6v1b))n1qw@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 # production
 ALLOWED_HOSTS = ['https://aabhishek.herokuapp.com/']
 
 # staging
-# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['localhost','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
@@ -83,7 +83,8 @@ DATABASES = {
         'NAME': 'portfolio',
         'USER': 'postgres',
         "PASSWORD": 'forth',
-        'HOST': 'https://aabhishek.herokuapp.com/'
+        # 'HOST': 'https://aabhishek.herokuapp.com/'
+        'HOST': 'localhost'
     }
 }
 
